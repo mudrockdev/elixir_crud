c4e9349a2973887f00faae94d4bc3e8d3f23b5d3
Ufuk Furkan Öztürk <ufukfurkanozturk@gmail.com>
2026-09-19 16:13:58 +0300

    base

---
+++ lib/elixir_crud.ex
@@ -0,0 +1,18 @@
+defmodule ElixirCrud do
+  @moduledoc """
+  Documentation for `ElixirCrud`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirCrud.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_crud_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirCrudTest do
+  use ExUnit.Case
+  doctest ElixirCrud
+
+  test "greets the world" do
+    assert ElixirCrud.hello() == :world
+  end
+end
